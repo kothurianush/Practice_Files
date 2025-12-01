a389cd99f9547c9742f602a0bb45747dc4778b3b
Anush Kothuri <heyanushkothuri@gmail.com>
2025-12-01 11:31:33 +0530

    basics

---
+++ strings.py
@@ -0,0 +1,14 @@
+#Strings
+#1.Indexing
+b="hello, world!"
+print(len(b))
+print(b[7])
+print(b[0],b[12])
+#slicing
+print(b[0:5])
+print(b[:3])
+print(b[6:])
+print(b[0:13])
+#negative indexing
+print(b[-5:-1])
+print(b[-3:])
