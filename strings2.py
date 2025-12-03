a14594efa06dcc1594529daeceda0d842d7fa32e
Anush Kothuri <heyanushkothuri@gmail.com>
2025-12-03 08:22:18 +0530

    palin

--- strings2.py
+++ strings2.py
@@ -13,3 +13,13 @@ rev=" "
 for ch in s:
     rev=ch+rev
 print(rev)
+
+#palindrome
+n="madam"
+rev=""
+for ch in n:
+    rev=ch+rev
+if n==rev:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
